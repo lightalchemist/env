bc6c7240f586376aaf280edceacbbc0758697239
hongwei <hongwei.ng@adsc.com.sg>
2015-04-16 17:07:42 +0800

    Added commands to update fresh install

---
+++ ubuntu/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# Script to setup ubuntu system.
+
+# Should consider changing shebang to use bash if using bash script
+# because there is a chance user is running other shell
+
+sudo apt-get update
+sudo apt-get dist-upgrade
