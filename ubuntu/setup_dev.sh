26d5a7036e5feaa81e34a18a204a688e6263c971
t7910 <t7910@adsc.com.sg>
2015-06-24 13:11:54 +0800

    Added libqt4-dev

--- ubuntu/setup_dev.sh
+++ ubuntu/setup_dev.sh
@@ -5,10 +5,6 @@
 
 # build essentials
 sudo apt-get install build-essential gcc gcc-doc g++ glibc-doc
-# libstdc++6 and dev version
-# libstdc++5
-# debug version also
-# Install documentation also
 
 # Editors
 sudo apt-get install vim vim-gtk vim-common vim-doc
@@ -24,8 +20,8 @@ sudo apt-get install subversion subversion-tools
 # Boost libraries
 sudo apt-get install libboost-all-dev
 
-# Parser stuff
-# sudo apt-get install xml-core
+# qt
+sudo apt-get install libqt4-dev
 
 # Documentation generators
 sudo apt-get install doxygen
