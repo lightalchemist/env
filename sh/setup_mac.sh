35562b2b943cf0598417f5510da05f0cc9218bfc
hongwei <lightalchemist@gmail.com>
2016-01-30 12:26:27 +0800

    Setup script for mac

---
+++ sh/setup_mac.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+git clone https://github.com/zsh-users/antigen ~/.antigen
+
+# TODO
+
+* Setup homebrew
+* Install zsh if not already installed
+* Install packages like coreutils
+* Install neovim
+brew install neovim/neovim/neovim
