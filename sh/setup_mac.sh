70ae232e3892a217bc7e23229883f90b93b65a51
hongwei <lightalchemist@gmail.com>
2016-02-14 22:14:05 +0800

    Added instructions for installing qt, boost, opencv

--- sh/setup_mac.sh
+++ sh/setup_mac.sh
@@ -5,6 +5,25 @@ git clone https://github.com/zsh-users/antigen ~/.antigen
 # TODO
 
 * Setup homebrew
+* See instructions here: http://brew.sh
+
 * Install zsh if not already installed
+* Setup zsh with antigen and oh-my-zsh
 * Install packages like coreutils
 * Install neovim
+
+# Install qt5
+# brew install qt5
+# brew cask install qt-creator
+# brew link --force qt5
+# brew install boost
+
+# Set up Qt Creator and Boost for OS X
+# https://ryanclouser.com/2015/07/16/OSX-Qt-Creator-Boost/
+
+# Setup OpenCV and other related libraries and tools
+# http://www.pyimagesearch.com/2015/06/15/install-opencv-3-0-and-python-2-7-on-osx/
+brew install cmake pkg-config
+brew install jpeg libpng libtiff openexr
+brew install eigen tbb
+
