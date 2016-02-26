8d9404ce91fa43e8e5b90c4bf4fca830ceb5c1a6
hongwei <lightalchemist@gmail.com>
2016-02-26 11:04:46 +0800

    Improved ycm config file and added stuff for qt 5

--- vim/.ycm_extra_conf.py
+++ vim/.ycm_extra_conf.py
@@ -1,73 +1,102 @@
+# Stolen from: http://jesrui.sdf-eu.org/ycm-config-for-qt5.html
 # Partially stolen from https://bitbucket.org/mblum/libgp/src/2537ea7329ef/.ycm_extra_conf.py
 # Extra stuff taken from: https://gist.github.com/locojay/4950253
+
 import os
 import ycm_core
-
-# When in doubt, or when things don't work, Google ycm config file.
-
-# These are the compilation flags that will be used in case there's no
-# compilation database set (by default, one is not set).
-# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
-flags = [
-    '-Wall',
-    '-Wextra',
-    # '-Werror',
-    # '-Wc++98-compat',
-    '-Wno-long-long',
-    '-Wno-variadic-macros',
-    '-fexceptions',
-    # Use this for OS X
-    # '-stdlib=libc++',
-    # Use this for linux
-    # '-stdlib=libstdc++',
-    '-I', 'include',
-    '-I', 'src',
-    '-I', '.',
-    # '-isystem',
-    '-isystem', '/usr/include',
-    '-isystem', '/usr/local/include',
-    # '-isystem', '/usr/include/c++/4.2.1',  # Need to change this for OS!
-    # '-isystem', '/usr/lib/gcc/x86_64-linux-gnu/4.8/include',
-    # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-    # language to use when compiling headers. So it will guess. Badly. So C++
-    # headers will be compiled as C headers. You don't want that so ALWAYS specify
-    # a "-std=<something>".
-    # For a C project, you would set this to something like 'c99' instead of
-    # 'c++11'.
-    '-std=c++11',
-    # ...and the same thing goes for the magic -x option which specifies the
-    # language that the files to be compiled are written in. This is mostly
-    # relevant for c++ headers.
-    # For a C project, you would set this to 'c' instead of 'c++'.
-    '-x', 'c++',
-    # This path will only work on OS X, but extra paths that don't exist are not
-    # harmful
-    # '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
-    # '-isystem', '/opt/local/include',
-]
-
-import sys
-if sys.platform == "darwin":
-    flags.extend(['-stdlib=libc++', # Use this for OS X
-        '-isystem', '/usr/include/c++/4.2.1',  # Need to change this for OS!
-        '-isystem', '/opt/local/include',])
-
-elif sys.platform.startswith("linux"):
-    flags.extend(['-stdlib=libstdc++',
-    '-isystem', '/usr/include/c++/4.8',  # Need to change this for OS!
-    ])
+from clang_helpers import PrepareClangFlags
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
 # more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
-#
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
-if compilation_database_folder:
-    database = ycm_core.CompilationDatabase(compilation_database_folder)
-else:
-    database = None
+
+# These are the compilation flags that will be used in case there's no
+# compilation database set.
+flags = [
+# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
+# language to use when compiling headers. So it will guess. Badly. So C++
+# headers will be compiled as C headers. You don't want that so ALWAYS specify
+# a "-std=<something>".
+# For a C project, you would set this to something like 'c99' instead of
+# 'c++11'.
+'-std=c++11',
+# ...and the same thing goes for the magic -x option which specifies the
+# language that the files to be compiled are written in. This is mostly
+# relevant for c++ headers.
+# For a C project, you would set this to 'c' instead of 'c++'.
+'-x',
+'c++',
+
+# Qt 5 stuff
+'-DQT_CORE_LIB',
+'-DQT_GUI_LIB',
+'-DQT_NETWORK_LIB',
+'-DQT_QML_LIB',
+'-DQT_QUICK_LIB',
+'-DQT_SQL_LIB',
+'-DQT_WIDGETS_LIB',
+'-DQT_XML_LIB',
+
+'-I', '/usr/lib/qt5/mkspecs/linux-clang',
+'-I', '/usr/include/qt5',
+'-I', '/usr/include/qt5/QtConcurrent',
+'-I', '/usr/include/qt5/QtCore',
+'-I', '/usr/include/qt5/QtDBus',
+'-I', '/usr/include/qt5/QtGui',
+'-I', '/usr/include/qt5/QtHelp',
+'-I', '/usr/include/qt5/QtMultimedia',
+'-I', '/usr/include/qt5/QtMultimediaWidgets',
+'-I', '/usr/include/qt5/QtNetwork',
+'-I', '/usr/include/qt5/QtOpenGL',
+'-I', '/usr/include/qt5/QtPlatformSupport',
+'-I', '/usr/include/qt5/QtPositioning',
+'-I', '/usr/include/qt5/QtScript',
+'-I', '/usr/include/qt5/QtScriptTools',
+'-I', '/usr/include/qt5/QtSql',
+'-I', '/usr/include/qt5/QtSvg',
+'-I', '/usr/include/qt5/QtTest',
+'-I', '/usr/include/qt5/QtUiTools',
+'-I', '/usr/include/qt5/QtV8',
+'-I', '/usr/include/qt5/QtWebKit',
+'-I', '/usr/include/qt5/QtWebKitWidgets',
+'-I', '/usr/include/qt5/QtWidgets',
+'-I', '/usr/include/qt5/QtXml',
+'-I', '/usr/include/qt5/QtXmlPatterns',
+
+'-I', '.',
+# '-I', 'Tests',
+# '-I', 'build',
+# '-I', 'build/Tests',
+
+# System include directories
+'-isystem', '/usr/include',
+'-isystem', '/usr/local/include',
+
+'-Wall',
+'-Wextra',
+# '-Werror',
+# '-Wc++98-compat',
+'-Wno-long-long',
+'-Wno-variadic-macros',
+'-fexceptions',
+
+]
+
+import sys
+if sys.platform == "darwin":
+    flags.extend([
+        '-stdlib=libc++',                      # Use this for OS X
+        '-isystem', '/usr/include/c++/4.2.1',  # Change this for system!
+    ])
+
+elif sys.platform.startswith("linux"):
+    flags.extend([
+        '-stdlib=libstdc++',
+        '-isystem', '/usr/include/c++/4.8'  # Need to change this for system!
+    ])
 
 
 if compilation_database_folder:
@@ -82,7 +111,7 @@ def DirectoryOfThisScript():
 
 def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
   if not working_directory:
-    return list( flags )
+    return flags
   new_flags = []
   make_next_absolute = False
   path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
@@ -114,9 +143,11 @@ def FlagsForFile( filename ):
     # Bear in mind that compilation_info.compiler_flags_ does NOT return a
     # python list, but a "list-like" StringVec object
     compilation_info = database.GetCompilationInfoForFile( filename )
-    final_flags = MakeRelativePathsInFlagsAbsolute(
-      compilation_info.compiler_flags_,
-      compilation_info.compiler_working_dir_ )
+    final_flags = PrepareClangFlags(
+        MakeRelativePathsInFlagsAbsolute(
+            compilation_info.compiler_flags_,
+            compilation_info.compiler_working_dir_ ),
+        filename )
   else:
     relative_to = DirectoryOfThisScript()
     final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
